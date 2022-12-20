ee1e8456e5651cdd79fef7ec9d96c61bd4324f1a
JarlBentzen <JarlBentzen@gmail.com>
2022-12-20 20:18:21 +0100

    last change

--- map.js
+++ map.js
@@ -36,7 +36,7 @@ const geojson = {
             },
             properties: {
                 title: 'Hvidhajen',
-                description: '<style> .mapboxgl-popup-content {background-image:url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/492/original/Mask_Group_2.png?1671392820" ); }</style><p>"The Great White Shark" is a mural painted by Eske Touborg, one of many painted by him in Aarhus.This is one is a part of his underwater collection. Vestergade 62, 8000 Aarhus, Danmark</p><br><h2>Eske Touborg</h2><br><p>Eske Touborg has painted graffiti and art since he was 11 years old. He is a strong believer that his best ideas come from good conversation and tries to make art that is meaningful to people. </p><div class="SoMe"><a href="https://www.facebook.com/EskeTM/"><i class="fa fa-facebook"></i></a><a href="https://www.esketoub.org/"><i class="fa fa-globe"></i></a><a href="https://www.instagram.com/esketouborg/?hl=en"><i class="fa fa-instagram"></i></a><a href="https://goo.gl/maps/U7ZEZjNWpHUQT1gY8"><i class="fa fa-map"></i></a></div>'
+                description: '<style> .mapboxgl-popup-content {background-image:url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/492/original/Mask_Group_2.png?1671392820" ); }</style><p>"The Great White Shark" is a mural painted by Eske Touborg, one of many painted by him in Aarhus.This is one is a part of his underwater collection.<br><br> Vestergade 62, 8000 Aarhus, Danmark</p><br><h2>Eske Touborg</h2><br><p>Eske Touborg has painted graffiti and art since he was 11 years old. He is a strong believer that his best ideas come from good conversation and tries to make art that is meaningful to people. </p><br><div class="SoMe"><a href="https://www.facebook.com/EskeTM/"><i class="fa fa-facebook"></i></a><a href="https://www.esketoub.org/"><i class="fa fa-globe"></i></a><a href="https://www.instagram.com/esketouborg/?hl=en"><i class="fa fa-instagram"></i></a><a href="https://goo.gl/maps/U7ZEZjNWpHUQT1gY8"><i class="fa fa-map"></i></a></div>'
             }
         },
         {
@@ -47,7 +47,7 @@ const geojson = {
             },
             properties: {
                 title: 'DONTLOOKBACKINANGER',
-                description: '<style> .mapboxgl-popup-content {background-image:url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/605/original/Mask_Group_4.png?1671443414"); }</style><p>The Mural "DONTLOOKBACKINANGER" is named after the song with the same name by the indie band "Oasis". Not much is known about this piece. While visiting here be sure to check out the delicatessen just across the street of the entrance to the courtyard. Klosterport 4t, 8000 Aarhus, Denmark</p><br><h2>Hendrik Beikirch</h2><br><p>Born In 1974, Hendrik Beikirch is a artist from Germany. His works are mostly black and white prominently Hendrik paints faces of older people. He has halso painted Asia\'s Tallest Mural in Busan, South Korea.</p><div class="SoMe"> <a href="https://www.facebook.com/ecbhendrikbeikirch/"><i class="fa fa-facebook"></i></a><a href="https://hendrikbeikirch.com/"><i class="fa fa-globe"></i></a><a href="https://www.instagram.com/explore/tags/hendrikbeikirch/?hl=en"><i class="fa fa-instagram"></i></a><a href="https://goo.gl/maps/gp9BC6Zg5GMGREGB6"><i class="fa fa-map"></i></a></div>'
+                description: '<style> .mapboxgl-popup-content {background-image:url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/605/original/Mask_Group_4.png?1671443414"); }</style><p>The Mural "DONTLOOKBACKINANGER" is named after the song with the same name by the indie band "Oasis". Not much is known about this piece. While visiting here be sure to check out the delicatessen just across the street of the entrance to the courtyard. <br><br>Klosterport 4t, 8000 Aarhus, Denmark</p><br><h2>Hendrik Beikirch</h2><br><p>Born In 1974, Hendrik Beikirch is a artist from Germany. His works are mostly black and white prominently Hendrik paints faces of older people. He has halso painted Asia\'s Tallest Mural in Busan, South Korea.</p><br><div class="SoMe"> <a href="https://www.facebook.com/ecbhendrikbeikirch/"><i class="fa fa-facebook"></i></a><a href="https://hendrikbeikirch.com/"><i class="fa fa-globe"></i></a><a href="https://www.instagram.com/explore/tags/hendrikbeikirch/?hl=en"><i class="fa fa-instagram"></i></a><a href="https://goo.gl/maps/gp9BC6Zg5GMGREGB6"><i class="fa fa-map"></i></a></div>'
             }
         },
         {
@@ -58,7 +58,7 @@ const geojson = {
             },
             properties: {
                 title: 'Faust',
-                description: '<style> .mapboxgl-popup-content {background-image:url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/606/original/Mask_Group_5.png?1671443422"); }</style>"Faust" was a painting during The 2008 "Icons for Now" exhibition. The artist of this mural is unknown but the mural is actually supposed to depict a "drooling vagina". Wile in the area make sure to see some of the thrift stores in the area like  red cross, visit the Salling rooftop, or go down the water/harbor area by Dokk1 Fredens Torv 6, 8000 Aarhus, Denmark <div class="SoMe"><a href="https://goo.gl/maps/qiaXTnejPGdvwHiu7"><i class="fa fa-map"></i></a></div>'
+                description: '<style> .mapboxgl-popup-content {background-image:url("https://s3.amazonaws.com/shecodesio-production/uploads/files/000/059/606/original/Mask_Group_5.png?1671443422"); }</style><p>"Faust" was a painting during The 2008 "Icons for Now" exhibition. The artist of this mural is unknown but the mural is actually supposed to depict a "drooling vagina". Wile in the area make sure to see some of the thrift stores in the area like  red cross, visit the Salling rooftop, or go down the water/harbor area by Dokk1 <br><br>Fredens Torv 6, 8000 Aarhus, Denmark </p><br><div class="SoMe"><a href="https://goo.gl/maps/qiaXTnejPGdvwHiu7"><i class="fa fa-map"></i></a></div>'
 
             }
         }
@@ -76,7 +76,7 @@ for (const feature of geojson.features) {
         .setPopup(
             new mapboxgl.Popup({ offset: 20 }) // add popups 
                 .setHTML(
-                    `<h2>${feature.properties.title}</h2><br><p>${feature.properties.description}</p>`
+                    `<div class="popupContent"><h2>${feature.properties.title}</h2><br><p>${feature.properties.description}</p></div>`
                 )
         )
         .addTo(map);
@@ -108,6 +108,32 @@ function openPopupAlt() {
 function closePopupAlt() {
     popupAlt.classList.remove("open-popupAlt");
 }
+
+
+
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+
+// Mobile burger menu toggle
+
+let burger = document.getElementById("hideburger");
+
+function openhideburger() {
+    burger.classList.toggle("openhideburger");
+}
+
+function closehideburger() {
+    burger.classList.remove("openhideburger");
+}
+
 
 // const popup = document.getElementsByClassName('popup');
 
